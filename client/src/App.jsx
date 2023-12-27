8a76b0fd9bf4c86acb17371ea44523cbe6e40439
0oAVIRALo0 <aviral21029@iiitd.ac.in>
2023-12-27 19:43:25 +0530

    Final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,62 @@
 import axios from "axios";
 import { saveAs } from "file-saver";
-import { useState } from "react";
+import { Component } from "react";
+
 import "./App.css";
 
-function App() {
-  const [name, setName] = useState("");
-
-  const handleOnChange = (event) => {
-    setName(event.target.value);
+class App extends Component {
+  state = {
+    name: "",
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
   };
 
-  const createAndDownloadPdf = () => {
-    axios.post("/create-pdf", name);
+  handleChange = ({ target: { value, name } }) =>
+    this.setState({ [name]: value });
+
+  createAndDownloadPdf = () => {
+    axios
+      .post("/create-pdf", this.state)
+      .then(() => axios.get("fetch-pdf", { responseType: "blob" }))
+      .then((res) => {
+        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+
+        saveAs(pdfBlob, "newPdf.pdf");
+      });
   };
 
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={handleOnChange}
-      />
-      <input type="number" placeholder="Receipt ID" />
-      <input type="text" placeholder="Price 1" />
-      <input type="text" placeholder="Price 2" />
-      <button onClick={createAndDownloadPdf}>Download PDF</button>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          onChange={this.handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Receipt ID"
+          name="receiptId"
+          onChange={this.handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Price 1"
+          name="price1"
+          onChange={this.handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Price 2"
+          name="price2"
+          onChange={this.handleChange}
+        />
+        <button onClick={this.createAndDownloadPdf}>Download PDF</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -3,16 +3,29 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const pdf = require('html-pdf');
 
-const pdfTemplate = require("./documents") 
+const pdfTemplate = require("./documents"); 
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = 8000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// POST - 
+// POST - PDF generationa and fetching of the data
+app.post("/create-pdf", (req, res) => {
+  pdf.create(pdfTemplate(req.body), {}).toFile("result.pdf", (err) => {
+    if (err) {
+      res.send(Promise.reject());
+    }
+    res.send(Promise.resolve());
+  })
+})
+
+// Get - Send the generated PDF to the client
+app.get("/fetch-pdf", (req, res) => {
+  res.sendFile(`${__dirname}/result.pdf`)
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); 
